f0a7e39910f72f656cf23145fd5dbca8b0a54773
Castillo316 <115067244+Castillo316@users.noreply.github.com>
2025-04-11 04:26:33 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simulación de interacción con botones de inicio de sesión y registro
+document.getElementById('loginBtn').addEventListener('click', () => {
+    alert('Redirigiendo al inicio de sesión...');
+  });
+  
+  document.getElementById('registerBtn').addEventListener('click', () => {
+    alert('Redirigiendo al registro...');
+  });
